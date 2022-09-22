4c6433be7940b30cc5ec84207ef6f8b3419ecb63
igorsilva <igor.silva@performait.com>
2022-09-22 16:31:06 -0300

    fix style file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 
-import styles from "./home.module.scss";
+import styles from "./styles.module.scss";
 
 interface HomeProps {
   product: {
